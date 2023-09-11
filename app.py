8f7bc113c963771219be148d1476a3d5faadf3a5
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2023-09-11 11:15:35 -0300

    "Update project dependencies and refactor code
    The change and refactor involve three crucial files: requirements.txt, app.py, and Jenkinsfile. We reduced the number of dependencies in the requirements.txt file for simplicity and eliminated unnecessary packages for optimal performance. We also refactored the app.py by fixing hard-coded values with environment variables, strengthening the security model and making the code more adaptive. Lastly, the Jenkinsfile has been heavily overhauled with more environment variables and new stages for better build pipeline structure. The changes are intended to optimize performance, increase security, and provide better CI/CD pipeline management."

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
+import os
 
-import pika as pika
 from flask import request
 
 import requests as requests
@@ -8,37 +8,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
-credentials = pika.PlainCredentials('guest', 'guest')
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='rabbitmq', credentials=credentials))
-channel = connection.channel()
-channel.queue_declare(queue='redirect', durable=True)
+url_repository = 'https://backend.csctracker.com/'
 
 
-@app.route('/notify-sync/message', methods=['POST'])
-def redirect_notify():  # put application's code here
-    jsn = request.get_json()
-    msg = {
-        'body': json.dumps(jsn, ensure_ascii=False),
-        'token': request.headers.get('Authorization'),
-        'url': '/notify-sync/message'
+@app.route('/api/v1/consumo', methods=['POST'])
+def consumo():  # put application's code here
+
+    headers = {
+        'Authorization': "Bearer " + os.environ['TOKEN_INTEGRACAO']
     }
-    print(msg)
-    channel.basic_publish(exchange='', routing_key='redirect', body=json.dumps(msg, ensure_ascii=False))
-    return "{}", 201, {'Content-Type': 'application/json'}
 
-
-@app.route('/backend/usage-info', methods=['POST'])
-def redirect_usage_info():  # put application's code here
-    jsn = request.get_json()
-    msg = {
-        'body': json.dumps(jsn, ensure_ascii=False),
-        'token': request.headers.get('Authorization'),
-        'url': '/backend/usage-info'
-    }
-    print(msg)
-    channel.basic_publish(exchange='', routing_key='redirect', body=json.dumps(msg, ensure_ascii=False))
-    return "{}", 201, {'Content-Type': 'application/json'}
+    args = request.args
+    body = {}
+    response = requests.post(f"{url_repository}monitor-consumo/consumo", headers=headers, json=body, params=args)
+    return "ok", 200, {'Content-Type': 'application/x-www-form-urlencoded'}
 
 
 if __name__ == '__main__':
