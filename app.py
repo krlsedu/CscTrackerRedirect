11cf6d01b8cff8a5a4071a41837b4ddb616a8066
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2023-09-14 14:43:12 -0300

    "Added dynamic routing to service endpoints
    This commit consists of the addition of dynamic routing methods to the application. This was done to enhance the flexibility of the app by handling requests with variable route parameters. Now, the app can redirect POST and GET requests to different services, ports, and any number of following parts, in a scalable and organized manner."

--- app.py
+++ app.py
@@ -24,5 +24,62 @@ def consumo():  # put application's code here
     return "ok", 200, {'Content-Type': 'application/x-www-form-urlencoded'}
 
 
+@app.route('/<service>/<port>/<part1>', methods=['POST'])
+def redirect1(service, port, part1):  # put application's code here
+
+    args = request.args
+    headers = request.headers
+    body = request.json
+    response = requests.post(f"http://{service}:{port}/{part1}", headers=headers, json=body, params=args)
+    return response, response.status_code, {'Content-Type': 'application/json'}
+
+
+@app.route('/<service>/<port>/<part1>', methods=['GET'])
+def redirectGet1(service, port, part1):
+    args = request.args
+    headers = request.headers
+    body = {}
+    response = requests.get(f"http://{service}:{port}/{part1}", headers=headers, json=body, params=args)
+    return response, response.status_code, {'Content-Type': 'application/json'}
+
+
+@app.route('/<service>/<port>/<part1>/<part2>', methods=['POST'])
+def redirect2(service, port, part1, part2):  # put application's code here
+
+    args = request.args
+    headers = request.headers
+    body = request.get_json()
+    response = requests.post(f"http://{service}:{port}/{part1}/{part2}", headers=headers, json=body, params=args)
+    return response, response.status_code, {'Content-Type': 'application/json'}
+
+
+@app.route('/<service>/<port>/<part1>/<part2>', methods=['GET'])
+def redirectGet2(service, port, part1, part2):
+    args = request.args
+    headers = request.headers
+    body = {}
+    response = requests.get(f"http://{service}:{port}/{part1}/{part2}", headers=headers, json=body, params=args)
+    return response, response.status_code, {'Content-Type': 'application/json'}
+
+
+@app.route('/<service>/<port>/<part1>/<part2>/<part3>', methods=['POST'])
+def redirect3(service, port, part1, part2, part3):  # put application's code here
+
+    args = request.args
+    headers = request.headers
+    body = request.get_json()
+    response = requests.post(f"http://{service}:{port}/{part1}/{part2}/{part3}", headers=headers, json=body, params=args)
+    return response, response.status_code, {'Content-Type': 'application/json'}
+
+
+@app.route('/<service>/<port>/<part1>/<part2>/<part3>', methods=['GET'])
+def redirectGet3(service, port, part1, part2, part3):
+    args = request.args
+    headers = request.headers
+    body = {}
+    response = requests.get(f"http://{service}:{port}/{part1}/{part2}/{part3}", headers=headers, json=body, params=args)
+    return response, response.status_code, {'Content-Type': 'application/json'}
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
