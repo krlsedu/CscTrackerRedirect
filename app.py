732d6d71c3f6b9071a099871dcfb1ed22651cca8
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2024-01-05 23:54:45 -0300

    Replace Flask and requests with csctracker modules in app.py
    Switched from using Flask and requests Python packages to the csctracker-py-core and csctracker-queue-scheduler libraries in app.py. This change involved modifying several method calls to use the more condensed and efficient csctracker methods. Also updated the Jenkinsfile to include the app's name in the version.txt file. Adjusted requirements.txt to reflect new dependencies.

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 import os
 
-import requests as requests
-from flask import Flask
-from flask import request
-from prometheus_flask_exporter import PrometheusMetrics
+from csctracker_py_core.starter import Starter
 
-app = Flask(__name__)
-
-metrics = PrometheusMetrics(app, group_by='endpoint', default_labels={'application': 'CscTrackerRedirect'})
-url_repository = 'https://backend.csctracker.com/'
+starter = Starter()
+app = starter.get_app()
+http_repository = starter.get_http_repository()
+backend_url = 'https://backend.csctracker.com/'
 
 
 @app.route('/api/v1/consumo', methods=['POST'])
@@ -18,71 +15,65 @@ def consumo():  # put application's code here
         'Authorization': "Bearer " + os.environ['TOKEN_INTEGRACAO']
     }
 
-    args = request.args
+    args = http_repository.get_args().args
     body = {}
-    response = requests.post(f"{url_repository}monitor-consumo/consumo", headers=headers, json=body, params=args)
+    response = http_repository.post(f"{backend_url}monitor-consumo/consumo", headers=headers, json=body, args=args)
     return "ok", 200, {'Content-Type': 'application/x-www-form-urlencoded'}
 
 
 @app.route('/<service>/<port>/<part1>', methods=['POST'])
 def redirect1(service, port, part1):  # put application's code here
 
-    args = request.args
-    headers = request.headers
-    body = request.json
-    response = requests.post(f"http://{service}:{port}/{part1}", headers=headers, json=body, params=args)
+    args = http_repository.get_args()
+    headers = http_repository.get_headers()
+    body = http_repository.get_json_body()
+    response = http_repository.post(f"http://{service}:{port}/{part1}", headers=headers, json=body, args=args)
     print(response.text)
     return response, response.status_code, {'Content-Type': 'application/json'}
 
 
 @app.route('/<service>/<port>/<part1>', methods=['GET'])
 def redirectGet1(service, port, part1):
-    args = request.args
-    headers = request.headers
-    body = {}
-    response = requests.get(f"http://{service}:{port}/{part1}", headers=headers, json=body, params=args)
+    args = http_repository.get_args()
+    headers = http_repository.get_headers()
+    response = http_repository.get(f"http://{service}:{port}/{part1}", headers=headers, params=args)
     return response.text
 
 
 @app.route('/<service>/<port>/<part1>/<part2>', methods=['POST'])
-def redirect2(service, port, part1, part2):  # put application's code here
-
-    args = request.args
-    headers = request.headers
-    body = request.get_json()
-    response = requests.post(f"http://{service}:{port}/{part1}/{part2}", headers=headers, json=body, params=args)
+def redirect2(service, port, part1, part2):
+    args = http_repository.get_args()
+    headers = http_repository.get_headers()
+    body = http_repository.get_json_body()
+    response = http_repository.post(f"http://{service}:{port}/{part1}/{part2}", headers=headers, json=body, args=args)
     return response.text
 
 
 @app.route('/<service>/<port>/<part1>/<part2>', methods=['GET'])
 def redirectGet2(service, port, part1, part2):
-    args = request.args
-    headers = request.headers
-    body = {}
-    response = requests.get(f"http://{service}:{port}/{part1}/{part2}", headers=headers, json=body, params=args)
+    args = http_repository.get_args()
+    headers = http_repository.get_headers()
+    response = http_repository.get(f"http://{service}:{port}/{part1}/{part2}", headers=headers, params=args)
     return response.text
 
 
 @app.route('/<service>/<port>/<part1>/<part2>/<part3>', methods=['POST'])
-def redirect3(service, port, part1, part2, part3):  # put application's code here
-
-    args = request.args
-    headers = request.headers
-    body = request.get_json()
-    response = requests.post(f"http://{service}:{port}/{part1}/{part2}/{part3}", headers=headers, json=body,
-                             params=args)
+def redirect3(service, port, part1, part2, part3):
+    args = http_repository.get_args()
+    headers = http_repository.get_headers()
+    body = http_repository.get_json_body()
+    response = http_repository.post(f"http://{service}:{port}/{part1}/{part2}/{part3}", headers=headers, json=body,
+                                    args=args)
     return response.text
 
 
 @app.route('/<service>/<port>/<part1>/<part2>/<part3>', methods=['GET'])
 def redirectGet3(service, port, part1, part2, part3):
-    args = request.args
-    headers = request.headers
-    body = None
+    args = http_repository.get_args()
+    headers = http_repository.get_headers()
     url = f"http://{service}:{port}/{part1}/{part2}/{part3}"
-    response = requests.get(url, headers=headers, json=body, params=args)
+    response = http_repository.get(url, headers=headers, params=args)
     return response.text
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+starter.start()
