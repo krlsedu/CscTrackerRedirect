cc08b00ac7607b0b144bb36ff921b4fba4164392
Carlos Eduardo Duarte Schwalm <carlosschwalm@frigelar.com.br>
2024-02-09 11:37:53 -0300

    Add simfrete route to app
    Implemented a new route '/simfrete' in the application that retrieves data from an external URL based on provided parameters. The function fetches parameters from the request, constructs the URL and makes a GET request, then returns the text in the response.

--- app.py
+++ app.py
@@ -11,16 +11,18 @@ starter = Starter()
 app = starter.get_app()
 http_repository = starter.get_http_repository()
 
+
 @app.route('/simfrete', methods=['GET'])
 def simfrete():
     cnpj = request.args.get('cnpj')
     dest = request.args.get('dest')
     notafiscal = request.args.get('notafiscal')
     url_ = f"https://frigelar.simfrete.com/consultaocorrv2.jsp?cnpj={cnpj}&dest={dest}&notafiscal={notafiscal}"
-    print(url_)
+    logging.getLogger().info(url_)
     result = requests.get(url_)
-    print(result.status_code)
+    logging.getLogger().info(result.status_code)
     return result.text
+
 
 @app.route('/api/v1/consumo', methods=['POST'])
 def consumo():  # put application's code here
